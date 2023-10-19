36452a6041c515b1413c879cae51bdf49af13c26
frarosset <37275581+frarosset@users.noreply.github.com>
2023-10-19 13:53:24 +0200

    Add toggle slider to keep the grid aspect ratio when modifying it
    A toggle slider has been added to keep the grid aspect ratio when
    either the number of rows or columns of the grid are modified.
    
    Note that as the elements of the grid are squares, such aspect
    ratio is not always the actual final one. In particular, when
    the number of columns is fixed, the number of rows is to be
    computed, and vice versa, and some rounding is applied.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let gridRows = 12;
 let gridCols = 12;
+let keepAR = false;
+let idealAR;
 
 let grid = document.querySelector('.grid');
 let gridCnt = document.querySelector('.grid-container');
@@ -9,9 +11,15 @@ let nColsSel = document.querySelector('#nColsSel');
 let nRowsLbl = document.querySelector('#nRowsLbl');
 let nColsLbl = document.querySelector('#nColsLbl');
 
+let keepARSel = document.querySelector('#keepARSel');
+let fixedARInfo = document.querySelector('#fixedARInfo');
+let gridInfo = document.querySelector('#gridInfo');
+
+gridInfo
 
 let gridDivs;
 
+/* Grid creation */
 function clearGrid(){
     removeDescendants(grid);
     gridDivs = [];
@@ -22,6 +30,10 @@ function removeDescendants(elem){
         removeDescendants(elem.lastChild)
         elem.removeChild(elem.lastChild);
     }
+}
+
+function setAspectRatio(elem, ar){
+    elem.style.aspectRatio = ar;
 }
 
 function createGrid(gridRows,gridCols){
@@ -51,23 +63,87 @@ function createGrid(gridRows,gridCols){
         grid.appendChild(row);
     }
 
-    gridDivs = document.querySelector('.elemOfGrid');;
+    gridDivs = document.querySelector('.elemOfGrid');
+
+    updateGridInfo();
 }
 
-function setAspectRatio(elem, ar){
-    elem.style.aspectRatio = ar;
-}
+
 
 createGrid(gridRows,gridCols);
 
-nRowsSel.addEventListener('input',(e) => {
-    gridRows = e.target.value;
-    nRowsLbl.textContent = gridRows;
-    createGrid(gridRows,gridCols);  
-})
-nColsSel.addEventListener('input',(e) => {
-    gridCols = e.target.value;
-    nColsLbl.textContent = gridCols;
-    createGrid(gridRows,gridCols); 
-})
 
+/* Settings interface*/
+
+nRowsSel.addEventListener('input',(e) => {
+    if (keepAR){
+        gridRows = e.target.value;
+        gridCols = Math.round(idealAR*gridRows);
+
+        if (gridCols>e.target.max){
+            gridCols=e.target.max;
+            gridRows = Math.round(gridCols/idealAR);
+            nRowsSel.value = gridRows;
+        } else if (gridCols<e.target.min){
+            gridCols=e.target.min;
+            gridRows = Math.round(gridCols/idealAR);
+            nRowsSel.value = gridRows;
+        }
+
+        nColsLbl.textContent = gridCols;
+        nRowsLbl.textContent = gridRows;
+        nColsSel.value = gridCols; 
+    } else {  
+        gridRows = e.target.value;
+        nRowsLbl.textContent = gridRows;   
+    }
+
+    createGrid(gridRows,gridCols); 
+});
+
+nColsSel.addEventListener('input',(e) => {
+    if (keepAR){
+        gridCols = e.target.value;
+        gridRows = Math.round(gridCols/idealAR);
+
+        if (gridRows>e.target.max){
+            gridRows=e.target.max;
+            gridCols = Math.round(idealAR*gridRows);
+            nColsSel.value = gridCols; 
+        } else if (gridRows<e.target.min){
+            gridRows=e.target.min;
+            gridCols = Math.round(idealAR*gridRows);
+            nColsSel.value = gridCols; 
+        }
+
+        nColsLbl.textContent = gridCols;
+        nRowsLbl.textContent = gridRows;
+        nRowsSel.value = gridRows; 
+    } else {  
+        gridCols = e.target.value;
+        nColsLbl.textContent = gridCols;   
+    }
+    createGrid(gridRows,gridCols);  
+});
+   
+keepARSel.addEventListener('input',(e) => {
+    idealAR = gridCols/gridRows;
+    keepAR = e.target.value=='1';
+
+    updateFixedARInfo();
+});
+
+function updateFixedARInfo(){
+    let idealARToPrec = idealAR.toPrecision(2);
+    let isApprox = idealAR != idealARToPrec;
+    let strAR = keepAR?(isApprox?'∼':'') + idealARToPrec + ' ('+gridCols + ':' + gridRows + ')': 'none';
+    fixedARInfo.textContent = strAR;
+}
+
+function updateGridInfo(){
+    let gridAR = gridCols/gridRows;
+    let gridARToPrec = gridAR.toPrecision(2);
+    let isApprox = gridAR != gridARToPrec;
+    let strAR = gridCols + 'x' + gridRows + ' (AR: ' + (isApprox?'∼':'') + gridARToPrec + ')';
+    gridInfo.textContent = strAR;
+}
